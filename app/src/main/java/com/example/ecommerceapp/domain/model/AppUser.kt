87d3e26317b72d349d3e07967df8b558d7911451
SilentKernel <rishabhks651@gmail.com>
2026-04-20 21:28:10 +0530

    Create AppUser.kt

---
+++ app/src/main/java/com/example/ecommerceapp/domain/model/AppUser.kt
@@ -0,0 +1,8 @@
+package com.example.ecommerceapp.domain.model
+
+data class AppUser(
+    val id: String = "",
+    val name: String = "",
+    val email: String = "",
+    val contact: String = ""
+)
