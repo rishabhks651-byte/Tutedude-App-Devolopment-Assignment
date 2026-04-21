acc30a77a4ea1325b300b64e010c92defc983c78
SilentKernel <rishabhks651@gmail.com>
2026-04-21 08:44:19 +0530

    Create Color.kt

---
+++ app/src/main/java/com/example/ecommerceapp/presentation/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.example.ecommerceapp.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Ink = Color(0xFF102542)
+val Coral = Color(0xFFF87060)
+val Sand = Color(0xFFF6E8EA)
+val Mist = Color(0xFFD9E2EC)
+val Slate = Color(0xFF243B53)
+val Snow = Color(0xFFFFFBFF)
